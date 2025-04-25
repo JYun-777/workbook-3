2d14878c763e0df054620e2e4288be28b13c9b63
JonY <jonathanwyun@gmail.com>
2025-04-25 13:34:07 -0700

    Format Dates Update
    Updated to show GMTTime

--- Mod-03/FormatDates/src/main/java/org/example/Main.java
+++ Mod-03/FormatDates/src/main/java/org/example/Main.java
@@ -2,25 +2,46 @@ package org.example;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.TimeZone;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
 
 
 
-    public static void main(String[] args){
-        TimeZone tz = TimeZone.getTimeZone("GMT");
+    public static void main(String[] args){// Get current time in UTC (GMT)
+
+        //get GMT time
+        ZonedDateTime gmtTime = ZonedDateTime.now(ZoneId.of("GMT"));
+
+        System.out.println(gmtTime);
+
         LocalDateTime currDateTime = LocalDateTime.now();
         System.out.println("Today is: " + currDateTime);
 
-        formatDateTime(currDateTime, "MM/dd/yyyy");
-        formatDateTime(currDateTime, "yyyy-MM-dd");
-        formatDateTime(currDateTime, "MMMM dd, yyyy");
-        formatDateTime(currDateTime, "E, MMM dd, yyyy hh:mm");
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nLOCAL TIME\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        formatLocalDateTime(currDateTime, "MM/dd/yyyy");
+        formatLocalDateTime(currDateTime, "yyyy-MM-dd");
+        formatLocalDateTime(currDateTime, "MMMM dd, yyyy");
+        formatLocalDateTime(currDateTime, "E, MMM dd, yyyy hh:mm");
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nGMT TIME\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        formatZonedDateTime(gmtTime, "MM/dd/yyyy");
+        formatZonedDateTime(gmtTime, "yyyy-MM-dd");
+        formatZonedDateTime(gmtTime, "MMMM dd, yyyy");
+        formatZonedDateTime(gmtTime, "E, MMM dd, yyyy hh:mm");
 
     }
 
-    public static void formatDateTime(LocalDateTime _dateTime,String _pattern){
+    public static void formatZonedDateTime(ZonedDateTime _dateTime, String _pattern){
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern(_pattern);
+        String formattedDate = _dateTime.format(fmt);
+        System.out.println(formattedDate);
+    }
+
+    public static void formatLocalDateTime(LocalDateTime _dateTime,String _pattern){
         DateTimeFormatter fmt = DateTimeFormatter.ofPattern(_pattern);
         String formattedDate = _dateTime.format(fmt);
         System.out.println(formattedDate);
