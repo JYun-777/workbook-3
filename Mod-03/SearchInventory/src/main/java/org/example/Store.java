8ab67c0a76caeb3e91fb91dbf9815a3e1e6d4d8d
JYun-777 <jonathanwyun@gmail.com>
2025-04-28 01:08:00 -0700

    SearchInventory added
    inventory csv file added

--- Mod-03/SearchInventory/src/main/java/org/example/Store.java
+++ Mod-03/SearchInventory/src/main/java/org/example/Store.java
@@ -1,10 +1,35 @@
 package org.example;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Store {
+    public static void main(String[] args) {
+        ArrayList<Product> inventory = getInventory();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("We carry the following inventory: ");
+        for (Product p : inventory) {
+            System.out.printf("id: %d %s - Price: $%.2f",
+                    p.getId(), p.getName(), p.getPrice());
+        }
+    }
+    public static ArrayList<Product> getInventory() {
+        ArrayList<Product> inventory = new ArrayList<Product>();
+        try{
+            FileReader fr = new FileReader("inventory.csv");
+            BufferedReader br = new BufferedReader(fr);
 
-
-
+            String dataLine;
+            String[] dataFields;
+            while(br.readLine() != null){
+                dataLine = br.readLine();
+                dataFields = dataLine.split("\\|");
+                Product newProduct = new Product (Integer.parseInt(dataFields[0]), dataFields[1], Float.parseFloat(dataFields[2]));
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        return inventory;
+    }
 }
