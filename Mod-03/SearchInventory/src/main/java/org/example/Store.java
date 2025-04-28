0b91dce5c479df7b2011a219b7aa120fa0dccf54
JonY <jonathanwyun@gmail.com>
2025-04-28 08:18:54 -0700

    SearchInventory reads csv

--- Mod-03/SearchInventory/src/main/java/org/example/Store.java
+++ Mod-03/SearchInventory/src/main/java/org/example/Store.java
@@ -10,7 +10,7 @@ public class Store {
         Scanner scanner = new Scanner(System.in);
         System.out.println("We carry the following inventory: ");
         for (Product p : inventory) {
-            System.out.printf("id: %d %s - Price: $%.2f",
+            System.out.printf("id: %d %s - Price: $%.2f\n",
                     p.getId(), p.getName(), p.getPrice());
         }
     }
@@ -26,6 +26,7 @@ public class Store {
                 dataLine = br.readLine();
                 dataFields = dataLine.split("\\|");
                 Product newProduct = new Product (Integer.parseInt(dataFields[0]), dataFields[1], Float.parseFloat(dataFields[2]));
+                inventory.add(newProduct);
             }
         }catch(IOException e){
             e.printStackTrace();
